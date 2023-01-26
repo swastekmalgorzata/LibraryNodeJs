5d560b3d5afc1791a7bd5d7ed564f66282d5ac46
Małgorzata Swastek <99887359+swastekmalgorzata@users.noreply.github.com>
2023-01-26 22:28:56 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,25 +1,25 @@
 const mongoose = require("mongoose");
-mongoose.connect("mongodb+srv://student:student@cluster0.re8z6.mongodb.net/air?retryWrites=true&w=majority")
 
-// importuję expressa
+
+
 const express = require("express");
 
-// importuję loggera
+
 const morgan = require("morgan");
 
-// importuję body parsera
+
 const bodyParser = require("body-parser");
 
-// tworzę instancję expresa
+
 const app = express();
 
-// uruchamiam logera
+
 app.use(morgan("combined"));
 
-// wyciągam część body i umieszczam w req.body
+
 app.use(bodyParser.json());
 
-// routy
+
 const bookRoutes = require("./routes/books");
 app.use("/", bookRoutes);
 
@@ -29,5 +29,4 @@ app.use((req, res) => {
 
 
 
-// eksport apki
 module.exports = app;
